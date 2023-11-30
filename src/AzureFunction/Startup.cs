fe650f4cf9365c9f703142f74f12a38e60b31904
Quyen Hua <httquyen@tma.com.vn>
2023-11-30 14:30:31 +0700

    Remove failed run

--- src/AzureFunction/Startup.cs
+++ src/AzureFunction/Startup.cs
@@ -3,9 +3,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 using Application;
 using Infrastructure;
-using Microsoft.Extensions.Logging;
-using OpenTelemetry;
-using Azure.Monitor.OpenTelemetry.Exporter;
 
 namespace FunctionApp;
 
@@ -23,22 +20,22 @@ public class Startup(IConfiguration configuration)
 
         // Create a new tracer provider builder and add an Azure Monitor trace exporter to the tracer provider builder.
         // It is important to keep the TracerProvider instance active throughout the process lifetime.
-        var tracerProvider = Sdk.CreateTracerProviderBuilder()
-            .AddAzureMonitorTraceExporter();
+        //var tracerProvider = Sdk.CreateTracerProviderBuilder()
+        //    .AddAzureMonitorTraceExporter();
 
-        // Add an Azure Monitor metric exporter to the metrics provider builder.
-        // It is important to keep the MetricsProvider instance active throughout the process lifetime.
-        var metricsProvider = Sdk.CreateMeterProviderBuilder()
-            .AddAzureMonitorMetricExporter();
+        //// Add an Azure Monitor metric exporter to the metrics provider builder.
+        //// It is important to keep the MetricsProvider instance active throughout the process lifetime.
+        //var metricsProvider = Sdk.CreateMeterProviderBuilder()
+        //    .AddAzureMonitorMetricExporter();
 
-        // Create a new logger factory.
-        // It is important to keep the LoggerFactory instance active throughout the process lifetime.
-        var loggerFactory = LoggerFactory.Create(builder =>
-        {
-            builder.AddOpenTelemetry(options =>
-            {
-                options.AddAzureMonitorLogExporter();
-            });
-        });
+        //// Create a new logger factory.
+        //// It is important to keep the LoggerFactory instance active throughout the process lifetime.
+        //var loggerFactory = LoggerFactory.Create(builder =>
+        //{
+        //    builder.AddOpenTelemetry(options =>
+        //    {
+        //        options.AddAzureMonitorLogExporter();
+        //    });
+        //});
     }
 }
